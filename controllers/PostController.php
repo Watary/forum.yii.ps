73d871d1c028624af972af1a87cc4e3705bacd73
Sergey Pedorenko <pedorenko.mail@gmail.com>
2017-05-16 17:51:22 +0300

    Add controller, models and view for Post

---
+++ controllers/PostController.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ADMIN
+ * Date: 09.05.2017
+ * Time: 13:43
+ */
+
+namespace app\controllers;
+
+use app\models\Post;
+use Yii;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use app\models\LoginForm;
+use app\models\ContactForm;
+
+class PostController extends Controller
+{
+
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['logout'],
+                'rules' => [
+                    [
+                        'actions' => ['logout'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    public function actionIndex(){
+        return $this->render('index');
+    }
+
+}
---
+++ models/Post.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ADMIN
+ * Date: 09.05.2017
+ * Time: 13:52
+ */
+
+namespace app\models;
+
+use yii;
+use yii\db\ActiveRecord;
+
+class Post extends ActiveRecord
+{
+
+    const STATUS_ACTIVE = 1;
+
+    public function getTopic($id){
+        return Post::find()
+            ->where(['id' => $id])
+            ->asArray()
+            ->one();
+    }
+
+    public function getPostList($id){
+        return Post::find()
+            ->where(['parent_id' => $id, 'status' => self::STATUS_ACTIVE])
+            ->asArray()
+            ->all();
+    }
+
+}
