ccef12dddc5273e3f29ed4f9ea37bbc7c051378c
Sergey Pedorenko <pedorenko.mail@gmail.com>
2017-05-21 14:13:34 +0300

    Update dump DataBase

--- forum_yii.sql
+++ forum_yii.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1:3306
--- Час створення: Трв 18 2017 р., 00:07
+-- Час створення: Трв 21 2017 р., 14:09
 -- Версія сервера: 5.7.13
 -- Версія PHP: 7.0.8
 
@@ -54,17 +54,22 @@ CREATE TABLE IF NOT EXISTS `post` (
   `id` int(11) NOT NULL,
   `text` text NOT NULL,
   `parent_id` int(11) NOT NULL,
-  `status` tinyint(1) NOT NULL DEFAULT '1'
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+  `status` tinyint(1) NOT NULL DEFAULT '1',
+  `author_id` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 --
 -- Дамп даних таблиці `post`
 --
 
-INSERT INTO `post` (`id`, `text`, `parent_id`, `status`) VALUES
-(1, 'Post 1', 1, 1),
-(2, 'Post 2', 1, 1),
-(3, 'Post 3', 1, 1);
+INSERT INTO `post` (`id`, `text`, `parent_id`, `status`, `author_id`) VALUES
+(1, 'Post 1', 1, 1, 0),
+(2, 'Post 2', 1, 1, 0),
+(3, 'Post 3', 1, 1, 0),
+(4, 'asdxczv', 1, 1, 0),
+(5, 'fdhdfgbxcbn', 1, 1, 0),
+(6, '123', 1, 1, 0),
+(7, '1', 1, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -86,6 +91,32 @@ CREATE TABLE IF NOT EXISTS `topic` (
 
 INSERT INTO `topic` (`id`, `title`, `parent_id`, `view`, `description`) VALUES
 (1, 'Topic 1', 1, 1, 'descript');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблиці `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `authKey` varchar(255) NOT NULL,
+  `accessToken` varchar(255) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+--
+-- Дамп даних таблиці `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `authKey`, `accessToken`) VALUES
+(1, '1', '11111', 'test1key', '1-token'),
+(2, 'login', 'password', 'authKey', 'login-accessToken'),
+(3, 'Qwerty', 'qwerty', '1', '1'),
+(4, 'User1', 'password1', 'authKey', 'accessToken-password1'),
+(5, 'User2', 'password2', 'RJRteJ0JJ_-z8-E2G0c1Ym9HS8HN5DN0', 'NHl3PJ7G7eO2zesti4NYre7oBRGBWanD'),
+(6, '123', '$2y$13$NmgHq.24LJb0WLUCS90YwOShURg7ZZZSe1UwJeRxdFmHs5AmmcOUq', 'pR3LNpBXOSPiA1qaMDSPy93-Lda_V7oj', 'YRu3Evz_ebGGXdoKZgm0v-ryWAcXZJVs');
 
 --
 -- Індекси збережених таблиць
@@ -110,6 +141,12 @@ ALTER TABLE `topic`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Індекси таблиці `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT для збережених таблиць
 --
 
@@ -122,12 +159,17 @@ ALTER TABLE `forum`
 -- AUTO_INCREMENT для таблиці `post`
 --
 ALTER TABLE `post`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT для таблиці `topic`
 --
 ALTER TABLE `topic`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT для таблиці `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
